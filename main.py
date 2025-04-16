f8a0fb5477d9556be39483367a8463a2561a3368
HASCODES <hasmasterofroblox@gmail.com>
2025-04-16 14:33:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import time, pygame as pg, random, math
+
+#setup word list
+wordNumbers = input("Enter the number of words to be displayed: ")
+print(wordNumbers)
+wordNumbers = int(wordNumbers)
+w=1
+word_list = []
+while w<wordNumbers:
+    word = input("Enter word " + str(w) + ": ")
+    word_list.append(word)
+    w=w+1
+
+#setup game settings
+randomizeWords = input("Would you like to randomize the order of the words? (y/n): ")
+if randomizeWords == "y":
+    randomizeWords = True
+elif randomizeWords == "n":
+    randomizeWords = False
+else:  
+    print("Invalid input, defaulting to no time recording.")
+    recordTime = False
+
+if randomizeWords == False:
+    w=1
+    while w<wordNumbers:
+        print("\n" + word_list[w] + "\n")
+        wordWrite = input("")
+        print(wordWrite)
+        if wordWrite == word_list[w]:
+            print("\nCorrect!")
+        else:
+            print("\nIncorrect!")
+        w=w+1
+    print("\nTest over.")
+elif randomizeWords == True:
+    word_list = random.sample(word_list, len(word_list))
+    w=1
+    while w<wordNumbers:
+        print("\n" + word_list[w] + "\n")
+        wordWrite = input("")
+        print(wordWrite)
+        if wordWrite == word_list[w]:
+            print("\nCorrect!")
+        else:
+            print("\nIncorrect!")
+        w=w+1
+    print("\nTest over.")
